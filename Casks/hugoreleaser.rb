8de2d49e56398ecb68df22d913ceb62b5794ec31
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2026-01-16 18:29:32 +0100

    Update hugoreleaser to v0.61.1

--- Casks/hugoreleaser.rb
+++ Casks/hugoreleaser.rb
@@ -1,13 +1,15 @@
 cask "hugoreleaser" do
-  version "0.61.0"
-  sha256 ""
+  version "0.61.1"
+  sha256 "d2fbe560031002cf68173276938b87bca2c15f238a111662a5b52fd8e5fa1586"
 
-  url "https://github.com/gohugoio/hugoreleaser/releases/download/v0.61.0/hugoreleaser_0.61.0_darwin-universal.pkg"
+  url "https://github.com/gohugoio/hugoreleaser/releases/download/v0.61.1/hugoreleaser_0.61.1_darwin-universal.pkg"
   name "hugoreleaser"
-  desc ""
-  homepage ""
+  desc "Build, archive and release Go programs."
+  homepage "https://github.com/gohugoio/hugoreleaser"
 
-  pkg "hugoreleaser_0.61.0_darwin-universal.pkg"
+  pkg "hugoreleaser_0.61.1_darwin-universal.pkg"
+
+  binary "/usr/local/bin/hugoreleaser"
 
   uninstall pkgutil: "io.gohugo.hugoreleaser"
 end
