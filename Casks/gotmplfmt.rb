6f3491b4115a98b2d2f1be1d88a6ae1c583e26b0
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2026-05-19 16:49:35 +0200

    Update gotmplfmt to v0.4.1

--- Casks/gotmplfmt.rb
+++ Casks/gotmplfmt.rb
@@ -1,13 +1,13 @@
 cask "gotmplfmt" do
-  version "0.4.0"
-  sha256 "e452b39d0e6f034b8f833e619ca3ed30bc85a5e793cacb19b1c2630166bffc64"
+  version "0.4.1"
+  sha256 "93b95a282b695a6506f8bb97f6e33a1625a4a48367b21f4ac53305fc47e7748c"
 
-  url "https://github.com/gohugoio/gotmplfmt/releases/download/v0.4.0/gotmplfmt_0.4.0_darwin-universal.pkg"
+  url "https://github.com/gohugoio/gotmplfmt/releases/download/v0.4.1/gotmplfmt_0.4.1_darwin-universal.pkg"
   name "gotmplfmt"
   desc ""
   homepage "https://github.com/gohugo/gotmplfmt"
 
-  pkg "gotmplfmt_0.4.0_darwin-universal.pkg"
+  pkg "gotmplfmt_0.4.1_darwin-universal.pkg"
 
   uninstall pkgutil: "io.gohugo.gotmplfmt"
 end
